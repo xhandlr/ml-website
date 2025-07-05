beac4b50e3759ac04bfe36ad1bb387917a7cc9ed
xhandlr <c.elgueta04@ufromail.cl>
2025-07-05 19:22:29 -0400

    chore: configurar Tailwind y crear componente Navbar
    - Corregir archivos .jsx a .tsx
    - Crear estructura inicial de Home.tsx

---
+++ src/components/TreeDiagram.tsx
@@ -0,0 +1,9 @@
+const TreeDiagram = () => {
+    return (
+        <div>
+            <h1>Ejemplo</h1>
+        </div>
+    );
+}
+
+export default TreeDiagram;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+        <div>
+            <div className="flex justify-center align-center">
+                <h2 className="text-2xl font-semibold text-blue-700">Aprende Machine Learning</h2>
+                <div className="bg-gray-600 width-400"></div>
+            </div>
+            <button>Empieza a aprender</button>
+        </div>
+    );
+};
+
+export default Home;
